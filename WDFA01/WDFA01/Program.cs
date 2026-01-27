7aaf97c3a7d8c96d46e1c30d675770a789f25b8d
OwenDdev <porewen3848@conestogac.on.ca>
2026-01-27 10:51:05 -0500

    SetClientNumber
    Set Client Number Method implemented
    
    An Async Method that  allows the user set the number of clients to write a message to a file

--- WDFA01/WDFA01/Program.cs
+++ WDFA01/WDFA01/Program.cs
@@ -1,5 +1,8 @@
 ﻿
 
+// Refrences : IEvangelist. (2024, June 3). Task-based asynchronous programming - .NET. Microsoft.com. https://learn.microsoft.com/en-us/dotnet/standard/parallel-programming/task-based-asynchronous-programming
+
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,21 +12,26 @@ class Program
 {
     //object used to do the syncronization 
     private static readonly object locker = new object();
+    private static volatile bool stopWriting = false;
+
     static void Main()
     {
         try
         {
+            string filepath = @"E:\SRC\WNP\-WNP-A01-Task-1\WDFA01\text.txt";
+            string message = "Hello";
+
             // Run multiple tasks that may throw exceptions
             Task task = Task.WhenAll(
-                Task.Run(() => MonnitorFileSize(1000, "C/log.txt")),
-                Task.Run(() => Writefile("log.txt", "Hello", 1)),
-                Task.Run(() => Setnumberofclients(200))
+                Task.Run(() => MonnitorFileSize(1000, filepath)),
+                //Task.Run(() => Writefile(filepath, message)),
+                Task.Run(() => Setnumberofclients(filepath, message, 200))
             );
 
-            // Wait for all tasks to complete (exceptions will be aggregated)
+            // Wait for all tasks to complete 
             task.Wait();
         }
-        catch (Exception Ex)
+        catch (Exception ex)
         {
             Console.WriteLine("Exception caught. Processing inner exceptions...\n");
 
@@ -36,11 +44,13 @@ class Program
         for (int i = 0; i < 100; i++)
         {
             // long length = new System.IO.FileInfo(path).Length;
-            long lenght = new FileInfo(filepath).Length;
-            Console.WriteLine($"file size: {lenght}");
+            long length = new FileInfo(filepath).Length;
+            Console.WriteLine($"file size: {length}");
 
-            if (lenght >= filesize)
+            if (length >= filesize)
             {
+                Console.WriteLine("File size Reached...");
+                stopWriting = true;
                 // how to stop async program 
                 break;
             }
@@ -50,7 +60,7 @@ class Program
         }
     }
 
-    static void Writefile(string filename, string message, int number)
+    static void Writefile(string filename, string message)
     {
         // code to write to file
          StreamWriter sw = null;
@@ -73,8 +83,26 @@ class Program
         }
     }
 
-    static void Setnumberofclients(int number)
+    static async Task Setnumberofclients(string filename, string message, int number)
     {
         // code to set number of clients 
+        Task[] clientTasks = new Task[number];
+
+        for (int i = 0; i < number; i++)
+        {
+            int clientId = i + 1; // for logging
+            clientTasks[i] = Task.Run(() =>
+            {
+                if (!stopWriting)
+                {
+                    Writefile(filename, message);
+                }
+                    
+            });
+        }
+
+        await Task.WhenAll(clientTasks);
+
+        Console.WriteLine("All clients have written their messages.");
     }
 }
--- WDFA01/WDFA01/obj/Debug/net8.0/WDFA01.AssemblyInfo.cs
+++ WDFA01/WDFA01/obj/Debug/net8.0/WDFA01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WDFA01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2084506fc697915682137f8562c5b2a5f4a5bdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("WDFA01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WDFA01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
