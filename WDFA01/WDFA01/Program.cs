59204454c343eed7b2ccaa53f36825d44daf438f
OwenDdev <porewen3848@conestogac.on.ca>
2026-01-29 12:22:36 -0500

    Tcp Communication
    TCP Client and Server Communication created

--- WDFA01/WDFA01/Program.cs
+++ WDFA01/WDFA01/Program.cs
@@ -8,101 +8,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Text;
+using System.ComponentModel.Design;
+using System.Net;
+
 class Program
 {
-    //object used to do the syncronization 
-    private static readonly object locker = new object();
-    private static volatile bool stopWriting = false;
-
-    static void Main()
+    public static async Task Main()
     {
-        try
+        bool done = false;
+        using TcpClient client = new TcpClient();
+        await client.ConnectAsync(IPAddress.Parse("10.144.99.100"), 5000);
+
+        // Create a NetworkStream
+        using NetworkStream stream = client.GetStream();
+
+        while (!done)
         {
-            string filepath = @"E:\SRC\WNP\-WNP-A01-Task-1\WDFA01\text.txt";
-            string message = "Hello";
+            // Convert the output stream to a byte array so TCP/IP
+            //   can use it
+            string message = Menu();
+            byte[] data = Encoding.UTF8.GetBytes(message);
 
-            // Run multiple tasks that may throw exceptions
-            Task task = Task.WhenAll(
-                Task.Run(() => MonnitorFileSize(1000, filepath)),
-                //Task.Run(() => Writefile(filepath, message)),
-                Task.Run(() => Setnumberofclients(filepath, message, 200))
-            );
+            await stream.WriteAsync(data, 0, data.Length);
+            Console.WriteLine("Message sent");
 
-            // Wait for all tasks to complete 
-            task.Wait();
+            // We need an input buffer.
+            // Then we can read the data.
+            byte[] buffer = new byte[1024];
+            int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+            // The input data is in bytes so we must convert it to
+            //    a string to use it.
+            string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine($"Server replied: {response}");
+
+            if (message == "Shutdown")
+            {
+                done = true;
+            }
+            
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Exception caught. Processing inner exceptions...\n");
-
-        }
+        Console.WriteLine("Program ended...press any key.");
+        Console.ReadKey();
     }
 
-    
-    static async Task MonnitorFileSize(int filesize, string filepath)
+    public static string Menu()
     {
-        for (int i = 0; i < 100; i++)
-        {
-            // long length = new System.IO.FileInfo(path).Length;
-            long length = new FileInfo(filepath).Length;
-            Console.WriteLine($"file size: {length}");
+        string clientNo;
+        string FileSize;
+        string Message;
+        string ClientMessage;
+        
+        Console.WriteLine("Input Number of clients:");
+        clientNo = Console.ReadLine();
+        Console.WriteLine("Input FileSize:");
+        FileSize = Console.ReadLine();
+        Console.WriteLine("Input message");
+        Message = Console.ReadLine();
 
-            if (length >= filesize)
-            {
-                Console.WriteLine("File size Reached...");
-                stopWriting = true;
-                // how to stop async program 
-                break;
-            }
+        ClientMessage = clientNo + "|" + FileSize + "|" + Message;
+        return ClientMessage;
 
-            // 10 times check per second
-            await Task.Delay(100);
-        }
-    }
-
-    static void Writefile(string filename, string message)
-    {
-        // code to write to file
-         StreamWriter sw = null;
-
-        lock (locker)
-        {
-            try
-            {
-                using (sw = new StreamWriter(filename, true))
-                {
-                    sw.WriteLine(message);
-                }
-            }
-            catch (Exception ex)
-            {
-
-                Console.WriteLine(ex.Message);
-            }
-
-        }
-    }
-
-    static async Task Setnumberofclients(string filename, string message, int number)
-    {
-        // code to set number of clients 
-        Task[] clientTasks = new Task[number];
-
-        for (int i = 0; i < number; i++)
-        {
-            int clientId = i + 1; // for logging
-            clientTasks[i] = Task.Run(() =>
-            {
-                if (!stopWriting)
-                {
-                    Writefile(filename, message);
-                }
-                    
-            });
-        }
-
-        await Task.WhenAll(clientTasks);
-
-        Console.WriteLine("All clients have written their messages.");
     }
 }
